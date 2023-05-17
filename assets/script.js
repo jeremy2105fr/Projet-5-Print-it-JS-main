dc3c9934ce91fc6bcb25edfe710ae475f1406591
Jeremy <jeremy2105@gmail.com>
2023-05-17 18:51:59 +0200

    Finalisation de Carousel + commentaire du code

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,36 @@
-// Attent le chargement du DOM
+// Attend le chargement du DOM
 document.addEventListener("DOMContentLoaded", function () {
-  //Gère les flèches et les boutons
+  // Variables
+  // Gère les flèches et les boutons
   const arrowLeft = document.getElementById("arrow-left");
   const arrowRight = document.getElementById("arrow-right");
-  const bot1 = document.getElementById("dot-1");
-  const bot2 = document.getElementById("dot-2");
-  const bot3 = document.getElementById("dot-3");
-  const bot4 = document.getElementById("dot-4");
-
-  // const slides = document.getElement(".banner-img");
+  const dot1 = document.getElementById("dot-1");
+  const dot2 = document.getElementById("dot-2");
+  const dot3 = document.getElementById("dot-3");
+  const dot4 = document.getElementById("dot-4");
+  const slides = [
+    {
+      image: "assets/images/slideshow/slide1.jpg",
+      tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+    },
+    {
+      image: "assets/images/slideshow/slide2.jpg",
+      tagLine:
+        "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    },
+    {
+      image: "assets/images/slideshow/slide3.jpg",
+      tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+    },
+    {
+      image: "assets/images/slideshow/slide4.png",
+      tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+    },
+  ];
+  // Définie la slide de "base" et permet de régler les slides suivantes
   let currentSlide = 1;
-  //Permet de limiter le nombre de slide de 1 à 4
+  // Fonctions
+  // Permet de limiter le nombre de slide de 1 à 4
   function limiteSlide() {
     if (currentSlide < 1) {
       currentSlide = 4;
@@ -20,79 +40,98 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     return currentSlide;
   }
-  // Permet de changer de slide
-  function changeSlide(currentSlide) {
-    const slides = [
-      {
-        image: "slide1.jpg",
-        tagLine:
-          "Impressions tous formats <span>en boutique et en ligne</span>",
-      },
-      {
-        image: "slide2.jpg",
-        tagLine:
-          "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-      },
-      {
-        image: "slide3.jpg",
-        tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-      },
-      {
-        image: "slide4.png",
-        tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
-      },
-    ];
-    const slide = slides[currentSlide - 1];
-    const image = document.createElement("img");
-    image.src = slide.image;
-    tagLineSlide.innerText = slide.tagLine;
-    const banner = document.getElementById("banner");
+  // Permet de changer les slides
+  function changeSlide() {
+    // Selectionne des élements html par l'ID
+    const banner = document.getElementById("texte");
     const carousel = document.getElementById("carousel");
-
-    const tagLineSlide = document.createElement("p");
-    banner.appendChild(tagLineSlide);
-    carousel.appendChild(image);
+    // Selectionne des élements du tableau
+    let image = slides[currentSlide - 1].image;
+    let tagLineSlide = slides[currentSlide - 1].tagLine;
+    // Remplacement des élements html par les élements du tableau
+    // Supprime l'image et en rajoute une nouvelle
+    carousel.innerHTML = "";
+    // Création d'une balise img
+    const imageSlide = document.createElement("img");
+    // Supprime le texte et en rajoute un nouveau
+    banner.innerHTML = "";
+    // Création d'une balise p
+    const tagLine = document.createElement("p");
+    // Redefinie la source de l'image
+    imageSlide.src = image;
+    // Redefinie le texte contenue dans la balise p
+    tagLine.innerHTML = tagLineSlide;
+    // Ajoute des élements html dans le DOM
+    banner.appendChild(tagLine);
+    carousel.appendChild(imageSlide);
   }
+  // Permet d'activer le changement de class des dots lors du changement de slide
+  function slideToDots() {
+    if (currentSlide === 1) {
+      dot1.classList.add("dot_selected");
+      dot2.classList.remove("dot_selected");
+      dot3.classList.remove("dot_selected");
+      dot4.classList.remove("dot_selected");
+    }
+    if (currentSlide === 2) {
+      dot1.classList.remove("dot_selected");
+      dot2.classList.add("dot_selected");
+      dot3.classList.remove("dot_selected");
+      dot4.classList.remove("dot_selected");
+    }
+    if (currentSlide === 3) {
+      dot1.classList.remove("dot_selected");
+      dot2.classList.remove("dot_selected");
+      dot3.classList.add("dot_selected");
+      dot4.classList.remove("dot_selected");
+    }
+    if (currentSlide === 4) {
+      dot1.classList.remove("dot_selected");
+      dot2.classList.remove("dot_selected");
+      dot3.classList.remove("dot_selected");
+      dot4.classList.add("dot_selected");
+    }
+  }
+  // Fonctions communes aux fleches et aux boutons
+  function dots() {
+    slideToDots(); // Appel de la fonction slideToDots
+    changeSlide(); // Appel de la fonction changeSlide
+    return currentSlide; // Retourne la nouvelle valeur à la slide actuelle
+  }
+  function arrows() {
+    limiteSlide(); // Permet l'appel de la fonction limiteSlide
+    dots(); // Appel de la fonction dots, qui possède déja les fonctions et la commande qui seront appelées ici
+  }
+
   // Fleches
   // Fleche gauche
   arrowLeft.addEventListener("click", function () {
     currentSlide--;
-    limiteSlide(); // Veiller a bien placer la fonction limiteSlide() apres le currentSlide--
-    console.log("====> curentSlide", currentSlide);
-    return currentSlide;
+    arrows(); // Permet l'appel de la fonction arrows qui est commune aux fleches
   });
   // Fleche droite
   arrowRight.addEventListener("click", function () {
     currentSlide++;
-    limiteSlide(); // Veiller a bien placer la fonction limiteSlide() apres le currentSlide++
-    console.log("====> curentSlide", currentSlide);
-    return currentSlide;
+    arrows();
   });
 
   //Boutons
-  bot1.addEventListener("click", function () {
+  dot1.addEventListener("click", function () {
     currentSlide = 1;
-    console.log("====> curentSlide", currentSlide);
-    return currentSlide;
+    dots(); // Permet l'appel de la fonction dots qui est commune aux boutons
   });
-
-  bot2.addEventListener("click", function () {
+  dot2.addEventListener("click", function () {
     currentSlide = 2;
-    console.log("====> curentSlide", currentSlide);
-    return currentSlide;
+    dots();
   });
-
-  bot3.addEventListener("click", function () {
+  dot3.addEventListener("click", function () {
     currentSlide = 3;
-    console.log("====> curentSlide", currentSlide);
-    return currentSlide;
+    dots();
   });
-
-  bot4.addEventListener("click", function () {
+  dot4.addEventListener("click", function () {
     currentSlide = 4;
-    console.log("====> curentSlide", currentSlide);
-    return currentSlide;
+    dots();
   });
-
+  // Appel initiale de la fonction changeSlide
   changeSlide();
 });
